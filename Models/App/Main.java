4f0739d1c4dee200990cdf9bdd3bb2589e7bf298
62905562+Developerheart@users.noreply.github.com <01Developerheart@users.noreplay.github.com>
2021-03-21 10:33:35 -0300

    Adicionando a classe main ao programa

--- Models/App/Main.java
+++ Models/App/Main.java
@@ -1,7 +1,36 @@
 package App;
 
+import Models.Entities.Reservation;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+
+        System.out.print("Number Room: ");
+        Integer numberRoom = sc.nextInt();
+
+        System.out.print("Check-In date (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+
+        System.out.print("Check-Out date(dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
+
+
+        Reservation novoC = new Reservation(numberRoom, checkIn, checkOut);
+
+        System.out.print(novoC.toString());
+
+
+
+        sc.close();
+
 
     }
 }
